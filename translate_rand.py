81ccf4ee15191929feab64053a1179c51c643c51
ytyz1307zzh <31958914+ytyz1307zzh@users.noreply.github.com>
2018-08-23 16:59:39 +0800

    top_k random sampling

---
+++ translate_rand.py
@@ -0,0 +1,66 @@
+''' Translate input text with trained model. '''
+
+import torch
+import argparse
+from tqdm import tqdm
+from transformer.Translator_rand import Translator_rand
+from DataLoader import DataLoader
+from preprocess import read_instances_from_file, convert_instance_to_idx_seq
+
+def main():
+    '''Main Function'''
+
+    parser = argparse.ArgumentParser(description='translate.py')
+
+    parser.add_argument('-model', required=True,
+                        help='Path to model .pt file')
+    parser.add_argument('-src', required=True,
+                        help='Source sequence to decode (one line per sequence)')
+    parser.add_argument('-vocab', required=True,
+                        help='preprocess file to provide vocabulary')
+    parser.add_argument('-output', default='pred.txt',
+                        help="""Path to output the predictions (each line will
+                        be the decoded sequence""")
+    parser.add_argument('-top_k', type=int, default=10,
+                        help='Top k random sample: each timestep select top_k probable words')
+    parser.add_argument('-batch_size', type=int, default=30,
+                        help='Batch size')
+    parser.add_argument('-no_cuda', action='store_true')
+
+    opt = parser.parse_args()
+    opt.cuda = not opt.no_cuda
+
+    # Prepare DataLoader
+    preprocess_data = torch.load(opt.vocab)
+    preprocess_settings = preprocess_data['settings']
+
+    test_src_word_insts = read_instances_from_file(
+        opt.src,
+        preprocess_settings.max_word_seq_len,
+        preprocess_settings.keep_case)
+
+    test_src_insts = convert_instance_to_idx_seq(
+        test_src_word_insts, preprocess_data['dict']['src'])
+
+    test_data = DataLoader(
+        preprocess_data['dict']['src'],
+        preprocess_data['dict']['tgt'],
+        src_insts=test_src_insts,
+        cuda=opt.cuda,
+        shuffle=False,
+        batch_size=opt.batch_size)
+
+    translator = Translator_rand(opt)
+    translator.model.eval()
+
+    print('[Info] Start translating...')
+    with open(opt.output, 'w') as f:
+        for batch in tqdm(test_data, mininterval=2, desc='  - (Test)', leave=False):
+            all_hyp = translator.translate_batch(batch)
+            for idx_seq in all_hyp:
+                pred_line = ' '.join([test_data.tgt_idx2word[idx] for idx in idx_seq])  #转化成单词拼接起来
+                f.write(pred_line + '\n')
+    print('[Info] Finished.')
+
+if __name__ == "__main__":
+    main()
